f38dcde2614dfc744bad2d7d1b94be40c772907c
shanquetta pelzer <kidkourier@shanquettas-MacBook-Air.local>
2020-10-06 10:06:49 -0500

    merged with master

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
 import logo from './logo.svg';
 import { LoginPage } from "./pages"
->>>>>>> 6e814a79d0e10af81c4a63ac6a16fcd0981c2e21
 import './App.css';
 import './firebase/config';
 import './pages/Signup';
@@ -13,20 +10,9 @@ import Signup from './pages/Signup';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <BrowserRouter>
-      <header></header>
-      <div className="app">
-        <Switch>
-          <Route exact path="/signup" component={Signup} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-=======
     <div className="App">
       <LoginPage />
     </div>
->>>>>>> 6e814a79d0e10af81c4a63ac6a16fcd0981c2e21
   );
 }
 

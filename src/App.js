9988c9941a111116aadd0313efdc90b15ef0e8a8
shanquetta pelzer <kidkourier@shanquettas-MacBook-Air.local>
2020-10-06 10:44:19 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import logo from './logo.svg';
 import { LoginPage } from "./pages"
 import './App.css';
 import './firebase/config';
-import './pages/Signup';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
-import Signup from './pages/Signup';
 
 
 function App() {

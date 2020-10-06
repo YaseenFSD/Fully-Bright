cb14b475b1f77b420a6b589c05ba0565104ce704
Reginald Jefferson <reginald.jeffersonm@gmail.com>
2020-10-06 09:37:26 -0500

    initial commit, working on navbar, routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import { LoginPage } from "./pages"
+import { NavBar } from "./components"
 import './App.css';
 import './firebase/config'
+
 
 function App() {
   return (
     <div className="App">
       <LoginPage />
+      <NavBar />
     </div>
+    
   );
 }
 

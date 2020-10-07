567dc23a4eda4bb065d551b259cb0c563e73cf2d
Shanquetta Pelzer <shanquetta.pelzer@outlook.com>
2020-10-07 22:08:51 +0000

    Revert "Merge branch 'login/signup-modal' into 'master'"
    This reverts merge request !7

--- src/pages/login-page/LoginPage.js
+++ src/pages/login-page/LoginPage.js
@@ -4,9 +4,8 @@ import { LoginForm, CreateUserForm } from "../../components"
 export function LoginPage() {
     return (
         <div className="LoginPage">
-           
             <LoginForm />
-            {/* <CreateUserForm /> */}
+            <CreateUserForm />
         </div>
     )
 }

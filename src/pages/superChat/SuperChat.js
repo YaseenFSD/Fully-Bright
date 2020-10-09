35bada311379eb0fa31b73019b100210e094a60a
Jonathane Leavell <jonathaneleavell@gmail.com>
2020-10-09 11:12:44 -0400

    finished basic functionality

--- src/pages/superChat/SuperChat.js
+++ src/pages/superChat/SuperChat.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState,useRef} from 'react'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
@@ -12,26 +12,69 @@ import { useSession } from '../../firebase/UserProvider'
 
 
 
+
 function SuperChat() {
     const [user] = useAuthState(auth)
     console.log(user)
-
-    const messagesRef = db.collection('chat')
-    const query =messagesRef.orderBy('createdAt').limit(25)
-    const [messages]=useCollectionData(query, {idField: 'id'})
-    console.log([messages])
-    return (
-        <div>
-            {messages && messages.map(msg=><ChatMessage key= {msg.id} message={msg}/>)}
-        </div>
-    )
-}
-function ChatMessage(props){
-    const {text,uid}=props.message
-    return(
-        <p>{text}</p>
-    )
+    const dummy = useRef();
+    const messagesRef = db.collection('messages');
+    const query = messagesRef.orderBy('createdAt').limit(25);
+  
+    const [messages] = useCollectionData(query, { idField: 'id' });
+  
+    const [formValue, setFormValue] = useState('');
+  
+  
+    const sendMessage = async (e) => {
+      e.preventDefault();
+  
+      const { uid, photoURL } = auth.currentUser;
+  
+      await messagesRef.add({
+        text: formValue,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        uid,
+        photoURL
+      })
+  
+      setFormValue('');
+      dummy.current.scrollIntoView({ behavior: 'smooth' });
     }
+  
+    return (<>
+      <main>
+  
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+  
+        <span ref={dummy}></span>
+  
+      </main>
+  
+      <form onSubmit={sendMessage}>
+  
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+  
+        <button type="submit" disabled={!formValue}>💡</button>
+  
+      </form>
+    </>)
+  }
+function ChatMessage(props){
+    const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || '/lightbulb.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
+}
+    
+
+    
+    
 
 export default SuperChat
 

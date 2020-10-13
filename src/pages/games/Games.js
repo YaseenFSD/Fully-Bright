b4c76a113dfc93bfa865895c5499507d4df172c2
Yaseen Al-Salamy <yhalsalamy@gmail.com>
2020-10-13 18:46:28 -0400

    creating random url for games now works

--- src/pages/games/Games.js
+++ src/pages/games/Games.js
@@ -1,8 +1,79 @@
-import React from "react"
+import React, { useRef, useEffect, useState } from "react"
+import { useQueryCache } from "react-query"
+import randomstring from "randomstring"
+import { auth, db } from "../../firebase"
+
+const randomFiveChars = () => randomstring.generate(5)
 
 // TODO let the current user choose a game and invite the email they want to play with
 export const Games = () => {
+    const [email, setEmail] = useState("")
+    const [isLoading, setLoading] = useState(true)
+    const [invitee, setInvitee] = useState("")
+    const [message, setMessage] = useState("")
+    const [randomCharacters, setRandomCharacters] = useState(randomFiveChars())
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                setEmail(user.email)
+                setLoading(false)
+            }
+        })
+    })
+    const getUserDocId = async (email) => {
+        email = email.trim()
+        let id = false
+        const usersCollection = await db.collection("users").get()
+        usersCollection.forEach((user) => {
+            // console.log(user.data().email)
+            if (user.data().email === email) {
+                id = user.id
+            }
+        })
+        return id
+    }
+
+    const createGameDoc = async (inviteeEmail, loggedInUserEmail) => {
+        let result = await db.collection("counterGame").doc(randomCharacters).get()
+        // console.log(result.exists)
+        while (result.exists) {
+            setRandomCharacters(randomFiveChars())
+            result = await db.collection("counterGame").doc(randomCharacters).get()
+        }
+        db.collection("counterGame").doc(randomCharacters).set({
+            players: [inviteeEmail, loggedInUserEmail]
+        })
+        console.log(randomCharacters)
+    }
+
+    const handleCounterInvite = async (event) => {
+        event.preventDefault()
+        // console.log()
+        setMessage("")
+        if (!invitee) {
+            setMessage("Please enter a valid email")
+            return
+        }
+        const docId = await getUserDocId(invitee)
+        if (!docId) {
+            setMessage("Email is not found")
+            return
+        }
+        console.log(window.location.host)
+        createGameDoc(email, invitee)
+        setMessage(`Game has been created at ${window.location.host}/game/${randomCharacters}`)
+        // console.log(inviteeDoc.data())
+    }
     return (<div className="gamesPage">
         render games to invite here
+
+        <div className="counterInvite">
+            <form onSubmit={handleCounterInvite}>
+                Counter game <br />
+                Invite to <input onChange={(e) => setInvitee(e.target.value)} type="text" placeholder="email" />
+                <button type="submit">Invite!</button>
+            </form>
+        </div>
+        {message}
     </div>)
 }

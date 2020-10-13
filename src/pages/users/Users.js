438ef5a41681ad40b3bc52817b74604d8d5fc8b5
shanquetta pelzer <kidkourier@shanquettas-MacBook-Air.local>
2020-10-13 13:49:33 -0500

    users updated code

--- src/pages/users/Users.js
+++ src/pages/users/Users.js
@@ -2,26 +2,35 @@ import React from 'react'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
-import {auth} from '../../firebase'
+import {auth, db} from '../../firebase'
 
 
 function listAllUsers(nextPageToken) {
-    // List batch of users, 1000 at a time.
-    auth.listUsers(1000, nextPageToken)
-      .then(function(listUsersResult) {
-        listUsersResult.users.forEach(function(userRecord) {
-          console.log('user', userRecord.toJSON());
-        });
-        if (listUsersResult.pageToken) {
-          // List next batch of users.
-          listAllUsers(listUsersResult.pageToken);
-        }
-      })
-      .catch(function(error) {
-        console.log('Error listing users:', error);
-      });
-  }
-  // Start listing users from the beginning, 1000 at a time.
-  listAllUsers();
+  //   // List batch of users, 1000 at a time.
+  //   auth.listUsers(1000, nextPageToken)
+  //     .then(function(listUsersResult) {
+  //       listUsersResult.users.forEach(function(userRecord) {
+  //         console.log('user', userRecord.toJSON());
+  //       });
+  //       if (listUsersResult.pageToken) {
+  //         // List next batch of users.
+  //         listAllUsers(listUsersResult.pageToken);
+  //       }
+  //     })
+  //     .catch(function(error) {
+  //       console.log('Error listing users:', error);
+  //     });
+  // }
+  // // Start listing users from the beginning, 1000 at a time.
+  // listAllUsers();
+
+  db.collection('users').get()
+  .then((data) =>   {
+    data.forEach((doc) => {
+      console.log(doc.data())
+    })
+  })
+  return null
+}
 
   export default listAllUsers

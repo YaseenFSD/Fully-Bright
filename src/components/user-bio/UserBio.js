a472e79ff5311820aa975b3b3f6641c3822e2aa1
Reginald Jefferson <reginald.jeffersonm@gmail.com>
2020-10-14 17:21:50 -0500

    will change as instructed for an empty text box

--- src/components/user-bio/UserBio.js
+++ src/components/user-bio/UserBio.js
@@ -1,9 +1,9 @@
+import { Button } from '@material-ui/core'
 import React, {useState, useEffect} from 'react'
 import {db, auth} from "../../firebase"
 
 export const UserBio = () => {
     const [currentEmail, setEmail] = useState("")
-    const [bio, setBio] = useState("")
     
     
     useEffect(()=>{
@@ -15,33 +15,61 @@ export const UserBio = () => {
         })
         findUserDoc()
         return() => unsubscribe()
-
+        
     })
     
     useEffect(()=>{
     },[])
+    
+    const findUserDoc = async () => {
+        let id
+        const collectionRef = await db.collection("users").get()
+        collectionRef.forEach((doc)=>{
+            console.log(doc.data().email)
+            console.log(currentEmail)
+            
+            if(doc.data().email === currentEmail) {
+                id = doc.id
+            }        
+        })
+        console.log(id)
+        return id
+    }
+    
+    // use a local state to keep track of what the useBio will be
+    const [bio, setBio] = useState("")
 
-const findUserDoc = async () => {
-    let id
-    const collectionRef = await db.collection("users").get()
-    collectionRef.forEach((doc)=>{
-        console.log(doc.data().email)
-        console.log(currentEmail)
-        
-        if(doc.data().email === currentEmail) {
-            id = doc.id
-        }        
-    })
-    console.log(id)
-    return id
-}
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
 
+    }
 
     return (
         <div>
-            
-            zzzz...
+
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={bio}
+          onChange={e => setBio(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
         </div>
+            
+            
     )
 }
+
+    
+
+
+    // onSubmit update the bio - .set a bio {merge:true}
+    //.set a bio {merge:true} keeps from overwriting
+
+
+
 

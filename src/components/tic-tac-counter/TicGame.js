4ee8da91ccb4787f8df7dd7babb74993944fb7f8
Yaseen Al-Salamy <yhalsalamy@gmail.com>
2020-10-14 20:58:30 -0400

    game now sends data to the server

--- src/components/tic-tac-counter/TicGame.js
+++ src/components/tic-tac-counter/TicGame.js
@@ -1,58 +1,69 @@
 import React, { useEffect, useState } from "react"
 import { db } from "../../firebase"
-import ReactHtmlParser from 'react-html-parser';
 import "./TicGame.css"
 
 export const TicGame = (props) => {
-    // const [game, setGame] = useState(null)
-    const [string, setString] = useState("")
+    const [game, setGame] = useState(null)
+    const [JSXstring, setJsxString] = useState(null)
     const [isLoading, setLoading] = useState(true)
+    const [message, setMessage] = useState("")
+    useEffect(() => {
+        getRealTimeData()
+        // testingUpdateGame()
+    }, [])
     useEffect(() => {
         renderBox()
-        // testingUpdateGame()
-        // getRealTimeData()
-    }, [])
+        return () => setJsxString(null)
+    })
     const gameRef = db.collection("counterGame").doc(props.gameId)
-    const game = {
-        "1": null,
-        "2": null,
-        "3": null,
-        "4": null,
-        "5": null,
-        "6": null,
-        "7": null,
-        "8": null,
-        "9": null,
+    // const game = {
+    //     "1": true,
+    //     "2": null,
+    //     "3": "asdf@gmail.com",
+    //     "4": null,
+    //     "5": null,
+    //     "6": null,
+    //     "7": null,
+    //     "8": null,
+    //     "9": null,
+    // }
+
+
+    const getRealTimeData = () => {
+        gameRef.onSnapshot((snapshot) => {
+            setGame(snapshot.data().game)
+            console.log(snapshot.data().game)
+        })
     }
+    const updateGame = async (dataKey) => {
+        // console.log(dataKey)
+        try {
+            await db.runTransaction(async (t) => {
 
-
-    // const getRealTimeData = () => {
-    //     gameRef.onSnapshot((snapshot) => {
-    //         setGame(snapshot.data().game)
-    //         console.log(snapshot.data().game)
-    //     })
-    // }
-    // const testingUpdateGame = async () => {
-    //     try {
-    //         await db.runTransaction(async (t) => {
-    //             const doc = await t.get(gameRef)
-    //             const placeValues = doc.data().game
-    //             t.update(gameRef, {
-    //                 game: {
-    //                     ...placeValues,
-    //                     "3": true
-    //                 }
-    //             })
-    //         })
-    //     } catch (error) {
-
-    //     }
-    // }
+                const doc = await t.get(gameRef)
+                const placeValues = doc.data().game
+                const newObj = { game: { ...placeValues } }
+                newObj.game[dataKey] = props.currentEmail
+                t.update(gameRef, newObj)
+            })
+            setMessage("")
+        } catch (error) {
+            setMessage("An error has occured")
+        }
+    }
     // console.log(Object.values(game))
     const handleCheckBox = async (event) => {
         event.persist()
-        console.log(event.currentTarget.dataset.key)
+        // console.log(event.currentTarget.dataset.key)
+        let dataKey = event.currentTarget.dataset.key
+        console.log(game[dataKey])
+
+        if (game[dataKey] !== null) {
+            setMessage("That spot is not empty")
+            return
+        }
         try {
+            updateGame(dataKey)
             // for (let key in game){
             //     if 
             // }
@@ -61,33 +72,38 @@ export const TicGame = (props) => {
 
         }
     }
+
     // let currentKeyIndex = gameKeys[0]
     const renderBox = () => {
-
+        console.log(game)
+        if (!game) {
+            return null
+        }
         for (let [key, value] of Object.entries(game)) {
-            console.log(value)
+            // console.log(value)
             if (!value) {
-                setString((prev) => {
-                    return prev + "<div className='ticBox' data-key='" + key +  "' onClick={(event) => handleCheckBox(event)}></div>"
+                setJsxString((prev) => {
+                    return <>{prev}<div className='ticBox' data-key={key} onClick={(event) => handleCheckBox(event)}></div></>
                 })
                 // (<div className="ticBox" data-key="nasdf" onClick={(event) => handleCheckBox(event)}></div>)
             } else if (value === props.currentEmail) {
-                setString((prev) => {
-                    return prev + `<div className='currentPlayer ticBox' data-key=${key}></div>`
+                setJsxString((prev) => {
+                    return <>{prev}<div className='currentPlayer ticBox' data-key={key} onClick={(event) => handleCheckBox(event)}></div></>
                 })
                 // return (<div className="currentPlayer ticBox"></div>)
             } else {
-                setString((prev) => {
-                    return prev + `<div className='otherPlayer ticBox' data-key=${key}></div>`
+                setJsxString((prev) => {
+                    return <>{prev}<div className='otherPlayer ticBox' data-key={key} onClick={(event) => handleCheckBox(event)}></div></>
                 })
                 // return (<div className="otherPlayer ticBox"></div>)
             }
         }
         setLoading(false)
     }
-    console.log(props)
+    // console.log(props)
     return (<div className="ticGame">
-        {!isLoading ? string : null}
+        {!isLoading ? JSXstring : null}
+        {message}
     </div>)
 }
-{/* {game ? <>{Object.values(game).map(renderBox)}</>: null} */}
+{/* {game ? <>{Object.values(game).map(renderBox)}</>: null} */ }
